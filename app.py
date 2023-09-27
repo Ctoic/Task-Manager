bf29ec8c8d079926daab876cc81c13e036296249
ctoic <i202612@nu.edu.pk>
2023-09-27 18:48:31 +0500

    created table in db

--- app.py
+++ app.py
@@ -17,15 +17,21 @@ class TodoModel(db.Model):
 
     def __repr__(self) -> str:
         return f"{self.id} - {self.title}"
-        
+
 
 @app.route('/')
 def Home():
+    todo = TodoModel(title="first name ", desc= "Start Investing in stock exchange")
+    db.session.add(todo)
+    db.session.commit()
+
     return render_template('index.html')
 
 # @route is a decorator which tells the app which URL should call the associated function 
-@app.route('/products')
+@app.route('/show')
 def Products():
+    alltodo= TodoModel.query.all()
+    print(alltodo)
     return ('Products')
 
 
