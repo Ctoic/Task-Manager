609dd66f7ac3f40f7b0647c1f3d983684ed3c2dc
ctoic <i202612@nu.edu.pk>
2023-09-27 18:30:06 +0500

    created database sqllite

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 # flask Minimal app code 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-# app is an instance of the Flask class which is a WSGI application
-# WSGI is a specification for a universal interface between the web server and the web application
 app = Flask(__name__)
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+# this is the model for the database    
+class TodoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.id} - {self.title}"
+        
+
+@app.route('/')
 def Home():
     return render_template('index.html')
 
@@ -16,8 +30,6 @@ def Products():
 
 
 # this is the main app which will run the app
-# __name__ is a special variable in python which is the name of the module
-# which is being run. If it is being run from the command line, then __name__ = __main__
 if __name__ == '__main__':
     app.run(debug=True)
 
