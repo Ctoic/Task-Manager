661676c3a6cbd0c523d7762155b35d1e8f110018
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2023-10-08 12:08:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# flask Minimal app code 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -8,74 +7,68 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# this is the model for the database    
+# Model for the database
 class TodoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     desc = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"{self.id} - {self.title}"
 
+# Input validation function
+def validate_input(input_string):
+    # You can customize this function to fit your specific validation needs
+    return input_string.strip()
 
 @app.route('/', methods=['GET', 'POST'])
 def Home():
     if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
+        title = validate_input(request.form['title'])
+        desc = validate_input(request.form['desc'])
         todo = TodoModel(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
 
-        
     alltodo = TodoModel.query.all()
     return render_template('index.html', alltodo=alltodo)
 
-# @route is a decorator which tells the app which URL should call the associated function 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        todo = TodoModel.query.filter_by(id=id).first()
-        todo.title = title
-        todo.desc = desc
-        db.session.add(todo)
-        db.session.commit()
+        title = validate_input(request.form['title'])
+        desc = validate_input(request.form['desc'])
+        todo = TodoModel.query.get(id)
+        if todo:
+            todo.title = title
+            todo.desc = desc
+            db.session.commit()
         return redirect(url_for('Home'))
-    todo = TodoModel.query.filter_by(id=id).first()
+    
+    todo = TodoModel.query.get(id)
     return render_template('update.html', todo=todo)
 
-@app.route('/delete/<int:id>')
-
+@app.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
-    todo = TodoModel.query.filter_by(id=id).first()
-    db.session.delete(todo)
-    db.session.commit()
+    todo = TodoModel.query.get(id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
     return redirect(url_for('Home'))
 
 @app.route('/search', methods=['GET'])
 def search():
-    if request.method == 'GET':
-        search_query = request.args.get('search')
-        search_query = f"%{search_query}%"  # Adding wildcard '%' for partial matching
+    search_query = request.args.get('search')
+    if search_query:
+        search_query = f"%{validate_input(search_query)}%"  # Adding wildcard '%' for partial matching
         todos = TodoModel.query.filter(TodoModel.title.like(search_query)).all()
         return render_template('search.html', todos=todos)
     return redirect(url_for('Home'))
-
-
-
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-    
-# this is the main app which will run the app
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
-
-
